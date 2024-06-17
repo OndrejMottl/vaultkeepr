41384a8e6a6848aab44eab60a59ca83c2e97da0b
OndrejMottl <ondrej.mottl@gmail.com>
2024-06-17 14:52:08 +0200

    `select_abiotic_var_by_name()`: Filter the data by abiotic variables name

---
+++ R/select_abiotic_var_by_name.R
@@ -0,0 +1,106 @@
+#' @title Select abiotic variables by name
+#' @description Filter the data by abiotic variables name
+#' @param con A class of `vault_pipe`
+#' @param sel_var_name A character vector of abiotic variable names
+#' @return A class of `vault_pipe`
+#' @export
+select_abiotic_var_by_name <- function(con = NULL, sel_var_name = NULL) {
+  .data <- rlang::.data
+
+  assertthat::assert_that(
+    inherits(con, "vault_pipe"),
+    msg = paste(
+      "`con` must be a class of `vault_pipe`",
+      "Use `open_vault()` to create a connection"
+    )
+  )
+
+  assertthat::assert_that(
+    all(names(con) %in% c("data", "db_con")),
+    msg = paste(
+      "con must have `data` and `db_con`",
+      "Use `open_vault()` to create a connection"
+    )
+  )
+
+  sel_data <- con$data
+
+  assertthat::assert_that(
+    inherits(sel_data, "tbl"),
+    msg = "data must be a class of `tbl`"
+  )
+
+  assertthat::assert_that(
+    "abiotic_variable_id" %in% colnames(sel_data),
+    msg = paste(
+      "The data does not contain `abiotic_variable_id` columns. Please add",
+      "`get_abiotic()` to the pipe before this function."
+    )
+  )
+
+  sel_con <- con$db_con
+
+  assertthat::assert_that(
+    inherits(sel_con, "SQLiteConnection"),
+    msg = "db_con must be a class of `SQLiteConnection`"
+  )
+
+  assertthat::assert_that(
+    "AbioticVariable" %in% DBI::dbListTables(sel_con),
+    msg = paste(
+      "AbioticVariable table does not exist in the Vault database",
+      "Make sure to connect to the correct database"
+    )
+  )
+
+  assertthat::assert_that(
+    "dataset_type" %in% colnames(sel_data),
+    msg = paste(
+      "The data should be filtered only for `gridpoints`.",
+      "However, the does not contain `dataset_type` columns. Please add",
+      "`select_dataset_by_type()` to the pipe before this function."
+    )
+  )
+
+  assertthat::assert_that(
+    "abiotic_variable_id" %in% colnames(dplyr::tbl(sel_con, "AbioticVariable")),
+    msg = paste(
+      "The AbioticVariable does not contain `abiotic_variable_id` column in the Vault database.",
+      "Make sure to connect to the correct database"
+    )
+  )
+
+  assertthat::assert_that(
+    is.character(sel_var_name),
+    msg = "`sel_var_name` must be a character vector"
+  )
+
+  dat_res <-
+    sel_data %>%
+    dplyr::left_join(
+      dplyr::tbl(sel_con, "AbioticVariable"),
+      by = "abiotic_variable_id"
+    ) %>%
+    dplyr::mutate(
+      keep = dplyr::case_when(
+        .default = TRUE,
+        !(.data$abiotic_variable_name %in% sel_var_name) &
+          .data$dataset_type == "gridpoints" ~ FALSE
+      )
+    ) %>%
+    dplyr::filter(
+      .data$keep == TRUE
+    ) %>%
+    dplyr::select(-"keep")
+
+  res <-
+    structure(
+      list(
+        data = dat_res,
+        db_con = sel_con
+      ),
+      class = c("list", "vault_pipe")
+    )
+
+  return(res)
+}
---
+++ tests/testthat/test-select_abiotic_var_by_name.R
@@ -0,0 +1,231 @@
+testthat::test_that("return correct class-high", {
+  test_datasets <-
+    open_vault(
+      path = paste(
+        tempdir(),
+        "example.sqlite",
+        sep = "/"
+      )
+    ) %>%
+    get_datasets() %>%
+    select_dataset_by_type("gridpoints") %>%
+    get_samples() %>%
+    get_abiotic() %>%
+    select_abiotic_var_by_name("temperature")
+
+  testthat::expect_s3_class(test_datasets, "vault_pipe")
+})
+
+testthat::test_that("basic data.frame structure", {
+  test_datasets <-
+    open_vault(
+      path = paste(
+        tempdir(),
+        "example.sqlite",
+        sep = "/"
+      )
+    ) %>%
+    get_datasets() %>%
+    select_dataset_by_type() %>%
+    get_samples() %>%
+    get_abiotic() %>%
+    select_abiotic_var_by_name("temperature")
+
+  testthat::expect_s3_class(test_datasets$data, "tbl_sql")
+})
+
+testthat::test_that("get only correct variable", {
+  test_datasets_values <-
+    open_vault(
+      path = paste(
+        tempdir(),
+        "example.sqlite",
+        sep = "/"
+      )
+    ) %>%
+    get_datasets() %>%
+    select_dataset_by_type("gridpoints") %>%
+    get_samples() %>%
+    get_abiotic() %>%
+    select_abiotic_var_by_name("temperature") %>%
+    purrr::chuck("data") %>%
+    dplyr::distinct(abiotic_variable_name) %>%
+    dplyr::collect() %>%
+    purrr::chuck("abiotic_variable_name")
+
+  testthat::expect_true(is.character(test_datasets_values))
+  testthat::expect_true(length(test_datasets_values) == 1)
+  testthat::expect_equal(test_datasets_values, "temperature")
+})
+
+testthat::test_that("get correct taxa for multi-taxa selection", {
+  test_datasets_values <-
+    open_vault(
+      path = paste(
+        tempdir(),
+        "example.sqlite",
+        sep = "/"
+      )
+    ) %>%
+    get_datasets() %>%
+    select_dataset_by_type("gridpoints") %>%
+    get_samples() %>%
+    get_abiotic() %>%
+    select_abiotic_var_by_name(
+      sel_var_name = c("temperature", "precipitation")
+    ) %>%
+    purrr::chuck("data") %>%
+    dplyr::distinct(abiotic_variable_name) %>%
+    dplyr::collect() %>%
+    purrr::chuck("abiotic_variable_name")
+
+  testthat::expect_true(is.character(test_datasets_values))
+  testthat::expect_true(length(test_datasets_values) == 2)
+  testthat::expect_equal(
+    test_datasets_values,
+    c("temperature", "precipitation")
+  )
+})
+
+testthat::test_that("subset dataset", {
+  test_datasets_full <-
+    open_vault(
+      path = paste(
+        tempdir(),
+        "example.sqlite",
+        sep = "/"
+      )
+    ) %>%
+    get_datasets() %>%
+    select_dataset_by_type("gridpoints") %>%
+    get_samples() %>%
+    get_abiotic()
+
+  test_datasets_sub <-
+    test_datasets_full %>%
+    select_abiotic_var_by_name("temperature")
+
+  nrows_full <-
+    test_datasets_full$data %>%
+    dplyr::count() %>%
+    dplyr::pull(n)
+
+  nrows_sub <-
+    test_datasets_sub$data %>%
+    dplyr::count() %>%
+    dplyr::pull(n)
+
+  testthat::expect_true(is.numeric(nrows_full))
+  testthat::expect_true(is.numeric(nrows_sub))
+  testthat::expect_true(nrows_full > 0)
+  testthat::expect_true(nrows_sub > 0)
+  testthat::expect_true(nrows_sub < nrows_full)
+})
+
+testthat::test_that("return empty dataset", {
+  nrows_empty <-
+    open_vault(
+      path = paste(
+        tempdir(),
+        "example.sqlite",
+        sep = "/"
+      )
+    ) %>%
+    get_datasets() %>%
+    select_dataset_by_type("gridpoints") %>%
+    get_samples() %>%
+    get_abiotic() %>%
+    # select a variable that does not exist
+    select_abiotic_var_by_name("pikachu") %>%
+    purrr::chuck("data") %>%
+    dplyr::count() %>%
+    dplyr::pull(n)
+
+  testthat::expect_equal(nrows_empty, 0)
+})
+
+# errors ----
+testthat::test_that("error wihtout `open_vault()`", {
+  testthat::expect_error(
+    select_abiotic_var_by_name("temperature")
+  )
+})
+
+testthat::test_that("error wihtout `get_datasets()`", {
+  testthat::expect_error(
+    open_vault(
+      path = paste(
+        tempdir(),
+        "example.sqlite",
+        sep = "/"
+      )
+    ) %>%
+      select_abiotic_var_by_name("temperature")
+  )
+})
+
+testthat::test_that("error wihtout `get_samples()`", {
+  testthat::expect_error(
+    open_vault(
+      path = paste(
+        tempdir(),
+        "example.sqlite",
+        sep = "/"
+      )
+    ) %>%
+      get_datasets() %>%
+      select_dataset_by_type("gridpoints") %>%
+      select_abiotic_var_by_name("temperature")
+  )
+})
+
+testthat::test_that("error wihtout `get_abiotic()`", {
+  testthat::expect_error(
+    open_vault(
+      path = paste(
+        tempdir(),
+        "example.sqlite",
+        sep = "/"
+      )
+    ) %>%
+      get_datasets() %>%
+      select_dataset_by_type("gridpoints") %>%
+      get_samples() %>%
+      select_abiotic_var_by_name("temperature")
+  )
+})
+
+testthat::test_that("error wihtout `select_dataset_by_type()`", {
+  testthat::expect_error(
+    open_vault(
+      path = paste(
+        tempdir(),
+        "example.sqlite",
+        sep = "/"
+      )
+    ) %>%
+      get_datasets() %>%
+      get_samples() %>%
+      get_abiotic() %>%
+      select_abiotic_var_by_name("temperature")
+  )
+})
+
+testthat::test_that("error with bad `sel_var_name` class", {
+  testthat::expect_error(
+    open_vault(
+      path = paste(
+        tempdir(),
+        "example.sqlite",
+        sep = "/"
+      )
+    ) %>%
+      get_datasets() %>%
+      select_dataset_by_type("gridpoints") %>%
+      get_samples() %>%
+      get_abiotic() %>%
+      select_abiotic_var_by_name(
+        sel_var_name = 123
+      )
+  )
+})
