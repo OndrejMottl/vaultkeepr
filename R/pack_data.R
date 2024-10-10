a710697206377df2be70dc00719c6a9fafe52bf3
Ondřej Mottl <ondrej.mottl@gmail.com>
2024-10-10 15:25:54 +0200

    pack_data: will atomaticaly pack all data types, based on the present col names

--- R/pack_data.R
+++ R/pack_data.R
@@ -1,6 +1,5 @@
 pack_data <- function(
     sel_data = NULL,
-    focus = c("vegetation", "traits"),
     verbose = TRUE) {
   assertthat::assert_that(
     inherits(sel_data, "tbl"),
@@ -8,60 +7,77 @@ pack_data <- function(
   )
 
   assertthat::assert_that(
-    is.character(focus),
-    msg = "The 'focus' must be a character"
-  )
-
-  focus <- match.arg(focus)
-
-
-  assertthat::assert_that(
-    focus %in% c("vegetation", "traits"),
-    msg = "The 'focus' must be one of the following: 'vegetation', 'traits'"
-  )
-
-  assertthat::assert_that(
     is.logical(verbose),
     msg = "The 'verbose' must be a logical"
   )
 
+  # dataset data -----
   vec_dataset_cols <-
-    intersect(
-      colnames(sel_data),
-      c(
-        "dataset_name", "data_source_desc",
-        "dataset_type", "dataset_source_type",
-        "coord_long", "coord_lat",
-        "sampling_method_details"
-      )
+    c(
+      "dataset_name", "data_source_desc",
+      "dataset_type", "dataset_source_type",
+      "coord_long", "coord_lat",
+      "sampling_method_details"
+    )
+
+  data_dataset <-
+    sel_data %>%
+    dplyr::select(
+      dplyr::all_of(vec_dataset_cols)
+    ) %>%
+    dplyr::distinct()
+
+  # sample metadata -----
+  vec_sample_meta_cols <-
+    c(
+      "sample_name", "age",
+      "sample_details", "sample_size", "description"
+    )
+
+  data_samples_meta <-
+    sel_data %>%
+    dplyr::select(
+      "dataset_name",
+      dplyr::any_of(vec_sample_meta_cols)
+    ) %>%
+    dplyr::arrange(.data$age) %>%
+    dplyr::distinct() %>%
+    tidyr::nest(
+      data_samples = dplyr::any_of(vec_sample_meta_cols)
+    )
+
+  data_packed <-
+    dplyr::left_join(
+      data_dataset,
+      data_samples_meta,
+      by = "dataset_name"
+    )
+
+  # community data -----
+  vec_sample_community_cols <-
+    c(
+      "sample_name",
+      "taxon_name", "value"
     )
 
   if (
-    focus == "vegetation"
+    all(vec_sample_community_cols %in% colnames(sel_data))
   ) {
-    data_packed <-
+    data_samples_community <-
       sel_data %>%
+      dplyr::select(
+        "dataset_name",
+        dplyr::any_of(vec_sample_community_cols)
+      ) %>%
+      tidyr::drop_na(.data$value) %>%
       tidyr::nest(
-        data_samples = c(
-          "sample_name", "age",
-          "sample_details", "sample_size", "description",
-          "taxon_name", "value"
-        )
+        data_community = dplyr::all_of(vec_sample_community_cols)
       ) %>%
       dplyr::mutate(
-        data_samples = purrr::map(
-          .progress = ifelse(verbose, "processing taxa", FALSE),
-          .x = .data$data_samples,
+        data_community = purrr::map(
+          .progress = ifelse(verbose, "Packing community data", FALSE),
+          .x = .data$data_community,
           .f = ~ .x %>%
-            dplyr::relocate(
-              dplyr::any_of(
-                c(
-                  "sample_name", "age",
-                  "sample_details", "sample_size", "description"
-                )
-              )
-            ) %>%
-            dplyr::arrange(age, taxon_name) %>%
             tidyr::pivot_wider(
               names_from = "taxon_name",
               values_from = "value",
@@ -69,47 +85,94 @@ pack_data <- function(
             )
         )
       )
-  } else {
-    taxon_column_name <- "taxon_name"
-
-    if (
-      "taxon_name_trait" %in% colnames(sel_data)
-    ) {
-      taxon_column_name <- "taxon_name_trait"
-    }
 
     data_packed <-
+      dplyr::left_join(
+        data_packed,
+        data_samples_community,
+        by = "dataset_name"
+      )
+  }
+
+  # trait data -----
+  vec_samples_trait_cols <-
+    c(
+      "sample_name",
+      "trait_domain_name", "trait_name",
+      "trait_value",
+      ifelse(
+        "taxon_name_trait" %in% colnames(sel_data),
+        "taxon_name_trait",
+        "taxon_name"
+      )
+    )
+
+  if (
+    all(vec_samples_trait_cols %in% colnames(sel_data))
+  ) {
+    data_samples_traits <-
       sel_data %>%
+      dplyr::select(
+        "dataset_name",
+        dplyr::any_of(vec_samples_trait_cols)
+      ) %>%
+      tidyr::drop_na(.data$trait_value) %>%
       tidyr::nest(
-        data_samples = c(
-          "sample_name", "age",
-          "sample_details", "sample_size", "description",
-          "trait_domain_name", "trait_name",
-          "taxon_name", "trait_value",
-          "value"
-        )
+        data_traits = dplyr::any_of(vec_samples_trait_cols)
       ) %>%
       dplyr::mutate(
-        data_samples = purrr::map(
-          .progress = ifelse(verbose, "processing taxa", FALSE),
-          .x = .data$data_samples,
+        data_traits = purrr::map(
+          .progress = ifelse(verbose, "Packing trait data", FALSE),
+          .x = .data$data_traits,
           .f = ~ .x %>%
-            dplyr::relocate(
-              dplyr::any_of(
-                c(
-                  "sample_name", "age",
-                  "sample_details", "sample_size", "description",
-                  "trait_domain_name", "trait_name"
-                )
-              )
-            ) %>%
-            dplyr::arrange(.data$age, taxon_column_name) %>%
             tidyr::pivot_wider(
-              names_from = taxon_column_name,
+              names_from = ifelse(
+                "taxon_name_trait" %in% colnames(sel_data),
+                "taxon_name_trait",
+                "taxon_name"
+              ),
               values_from = "trait_value",
-              values_fill = 0
+              values_fill = NA_real_
             )
         )
+      )
+
+    data_packed <-
+      dplyr::left_join(
+        data_packed,
+        data_samples_traits,
+        by = "dataset_name"
+      )
+  }
+
+  # abiotic data -----
+  vec_sample_abiotic_cols <-
+    c(
+      "sample_name",
+      "abiotic_variable_name", "abiotic_value",
+      "abiotic_variable_unit", "measure_details",
+      "sample_name_gridpoint"
+    )
+
+  if (
+    all(vec_sample_abiotic_cols %in% colnames(sel_data))
+  ) {
+    data_abiotic <-
+      sel_data %>%
+      dplyr::select(
+        "dataset_name",
+        dplyr::any_of(vec_sample_abiotic_cols)
+      ) %>%
+      tidyr::drop_na(.data$abiotic_value) %>%
+      tidyr::nest(
+        data_abiotic = dplyr::any_of(vec_sample_abiotic_cols)
+      )
+
+    data_packed <-
+      dplyr::left_join(
+        data_packed,
+        data_abiotic,
+        by = "dataset_name"
       )
   }
 
